b2a661536c6bf2173d949d15ae8431fa17cc7783
vcarvalho0 <vitor1.santos00@gmail.com>
2024-02-13 17:04:11 -0300

    feat: add prisma connection and database config

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import z from "zod";
 
 const envSchema = z.object({
-  PORT: z.coerce.string()
+  PORT: z.coerce.number().default(8080)
 });
 
 const envServer = envSchema.safeParse(process.env);
