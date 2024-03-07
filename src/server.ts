88ba43efa7633469475f8ca60e32c892289e4dd8
vcarvalho0 <vitor1.santos00@gmail.com>
2024-03-06 22:17:51 -0300

    chore: improve close server function

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import express, { Application } from "express";
 import cors from "cors";
 import { env } from "./env";
+import http from "http";
 import { configureRouter } from "./routes";
 import * as database from "./database";
 import logger from "./logger";
 
 export class ServerSetup {
   private express: express.Application;
+  private server: http.Server;
 
   constructor() {
     this.express = express();
+    this.server = http.createServer(this.express);
   }
 
   public async init(): Promise<void> {
@@ -19,7 +22,11 @@ export class ServerSetup {
   }
 
   private middlewareSetup(): void {
-    this.express.use(cors());
+    this.express.use(
+      cors({
+        origin: "*"
+      })
+    );
     this.express.use(express.json());
 
     configureRouter(this.express);
@@ -35,10 +42,21 @@ export class ServerSetup {
 
   public async close(): Promise<void> {
     await database.close();
+    if (this.server) {
+      await new Promise((resolve, reject) => {
+        this.server.close((err) => {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve(true);
+        });
+      });
+    }
   }
 
   public start(): void {
-    this.express.listen(env.PORT, () => {
+    this.server.listen(env.PORT, () => {
       logger.info(`Server is running at ${env.PORT}`);
     });
   }
