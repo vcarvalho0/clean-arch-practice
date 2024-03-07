50a9a55918da65def5ac4d4bf7448a6b2c544296
vcarvalho0 <vitor1.santos00@gmail.com>
2024-03-06 22:25:53 -0300

    feat: add graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,24 @@ const startServer = async (): Promise<void> => {
     const server = new ServerSetup();
     await server.init();
     server.start();
+
+    const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGQUIT"];
+
+    for (const exitSigal of exitSignals) {
+      process.on(exitSigal, async () => {
+        try {
+          await server.close();
+          logger.info("Server closed");
+          process.exit(1);
+        } catch (err) {
+          logger.error(`Server exited with error ${err}`);
+          process.exit(0);
+        }
+      });
+    }
   } catch (error) {
-    logger.error(`Something went wrong ${error}`);
+    logger.error(`Something unexpected happened ${error}`);
+    process.exit(0);
   }
 };
 
