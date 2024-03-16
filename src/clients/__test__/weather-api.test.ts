0356a804b20f03c411d03f71fc076dbd51a6259d
vcarvalho0 <vitor1.santos00@gmail.com>
2024-03-15 22:20:00 -0300

    feat: improve test with fixtures

--- src/clients/__test__/weather-api.test.ts
+++ src/clients/__test__/weather-api.test.ts
@@ -1,20 +1,22 @@
 import { describe, expect, it, vitest } from "vitest";
 import { WeatherAPI } from "../weather-api";
+import weatherFixture from "../../../test/fixtures/weather.json";
+import weatherFixtureNormalized from "../../../test/fixtures/weather_normalized.json";
 import axios from "axios";
 
 vitest.mock("axios");
 
 describe("WeatherAPI tests", () => {
-  it("Should serialize data", async () => {
+  it("Should normalize data", async () => {
     const lat = -23.583988;
     const lon = -46.466173;
 
-    axios.get = vitest.fn().mockResolvedValue({});
+    axios.get = vitest.fn().mockResolvedValue(weatherFixture);
 
     const weatherAPI = new WeatherAPI(axios);
 
     const response = await weatherAPI.fetch(lat, lon);
 
-    expect(response).toEqual({});
+    expect(response).toEqual(weatherFixtureNormalized);
   });
 });
