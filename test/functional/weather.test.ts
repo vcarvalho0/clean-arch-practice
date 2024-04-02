fad5588b347a993183d29e078be65279fce9f3c7
vcarvalho0 <vitor1.santos00@gmail.com>
2024-04-01 21:54:30 -0300

    chore: improve weather functional test

--- test/functional/weather.test.ts
+++ test/functional/weather.test.ts
@@ -1,40 +1,56 @@
 import { ServerSetup } from "@/server";
-import { beforeAll, describe, expect, it } from "vitest";
+import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
 import request from "supertest";
+import { UserPrismaDBRepository } from "@/repositories/prisma/prisma-user-repository";
+import JwtService from "@/services/jwt";
+import { PlacePrismaDBRepository } from "@/repositories/prisma/prisma-place-repository";
+import weatherNormalizedFixture from "../fixtures/weather_normalized.json";
 
 describe("Weather tests", () => {
   let server: ServerSetup;
 
+  const defaultUser = {
+    username: "John Doe",
+    email: "johndoe@email.com",
+    password: "123456"
+  };
+
+  const places = new PlacePrismaDBRepository();
+  const usersRepository = new UserPrismaDBRepository();
   beforeAll(async () => {
     server = new ServerSetup();
 
     await server.init();
   });
 
-  it("Should return weather data", async () => {
-    const { body, status } = await request(server.getExpress()).get(
-      "/forecast"
-    );
+  let token: string;
+  beforeEach(async () => {
+    const user = await usersRepository.create(defaultUser);
+    token = JwtService.generateToken(user.id);
 
-    expect(status).toBe(200);
-    expect(body).toEqual({
+    const defaultPlace = {
       name: "London",
       country: "United Kingdom",
       lat: 51.52,
       lon: -0.11,
-      temp_c: 9,
-      temp_f: 48.2,
-      condition: {
-        text: "Clear",
-        icon: "//cdn.weatherapi.com/weather/64x64/night/113.png",
-        code: 1000
-      },
-      wind_mph: 6.9,
-      wind_kph: 11.2,
-      wind_direction: "SW",
-      feelslike_c: 7.2,
-      feelslike_f: 45,
-      uv: 0
-    });
+      userId: user.id
+    };
+
+    await places.deleteAllPlaces();
+    await places.create(defaultPlace);
+  });
+
+  afterEach(async () => {
+    await places.deleteAllPlaces();
+    await usersRepository.deleteAll();
+  });
+
+  it("Should return weather data", async () => {
+    const { body, status } = await request(server.getExpress())
+      .get("/places/weather")
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(status).toBe(200);
+    expect(body).toEqual(weatherNormalizedFixture);
   });
 });
